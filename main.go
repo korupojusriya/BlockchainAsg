9fb18815c840aa5443b3057b1412af077bd265f6
korupojusriya <83969756+korupojusriya@users.noreply.github.com>
2023-06-12 11:13:17 +0530

    Update main.go by setting tpb and tottrans as env variables

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"os"
 	"sync"
 	"time"
-
+    "strconv"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
@@ -172,47 +173,39 @@ func main() {
 	}
 	defer db.Close()
 
-	for i := 1; i <= 10000; i++ {
-		key := fmt.Sprintf("SIM%d", i)
-		value := Entry{
-			Val:   float64(i),
-			Ver:   1.0,
-			Valid: true,
-		}
-
-		hashInput := fmt.Sprintf("%s:%f", key, value.Val)
-		hash := sha256.Sum256([]byte(hashInput))
-		value.Hash = fmt.Sprintf("%x", hash)
-
-		data, err := json.Marshal(value)
-		if err != nil {
-			log.Fatal("Error serializing entry:", err)
-		}
-
-		err = db.Put([]byte(key), data, nil)
-		if err != nil {
-			log.Fatal("Error inserting entry into LevelDB:", err)
-		}
+	args := os.Args[1:]
+	if len(args) < 2 {
+		fmt.Println("Please provide values for TRANSACTIONS_PER_BLOCK and TOTAL_TRANSACTIONS.")
+		return
+	}
+	transactionsPerBlock, err := strconv.Atoi(args[0])
+	if err != nil || transactionsPerBlock <= 0 {
+		fmt.Println("Invalid value for TRANSACTIONS_PER_BLOCK.")
+		return
 	}
 
-	transactionsPerBlock := 1000
-	totalTransactions := 10000
+	totalTransactions, err := strconv.Atoi(args[1])
+	if err != nil || totalTransactions <= 0 {
+		fmt.Println("Invalid value for TOTAL_TRANSACTIONS.")
+		return
+	}
+
 	totalBlocks := int(math.Ceil(float64(totalTransactions) / float64(transactionsPerBlock)))
 
-	prevBlockHash := "0xabc123" 
+	prevBlockHash := "0xabc123"
 	blockWriter := make(BlockWriter)
 
 	go func() {
-        var blocks []MyBlock
-        for block := range blockWriter {
-            blocks = append(blocks, block)
-            err := writeBlocksToFile(blocks)
-            if err != nil {
-                fmt.Println("Error writing blocks to file:", err)
-                return
-            }
-        }
-    }()
+		var blocks []MyBlock
+		for block := range blockWriter {
+			blocks = append(blocks, block)
+			err := writeBlocksToFile(blocks)
+			if err != nil {
+				fmt.Println("Error writing blocks to file:", err)
+				return
+			}
+		}
+	}()
 
 	for blockNumber := 1; blockNumber <= totalBlocks; blockNumber++ {
 		block := MyBlock{
@@ -222,9 +215,9 @@ func main() {
 			Txns:          make(map[string]Transaction),
 		}
 
-        block.ProcessingTime = time.Since(startTime)
-		startIndex := (blockNumber - 1) * transactionsPerBlock+1
-		endIndex := blockNumber*transactionsPerBlock
+		block.ProcessingTime = time.Since(startTime)
+		startIndex := (blockNumber - 1) * transactionsPerBlock + 1
+		endIndex := blockNumber * transactionsPerBlock
 		if endIndex > totalTransactions {
 			endIndex = totalTransactions
 		}
@@ -261,40 +254,39 @@ func main() {
 		}
 
 		block.UpdateBlockStatus(Committed)
-		blockWriter.WriteBlock(block) 
+		blockWriter.WriteBlock(block)
 		prevBlockHash = fmt.Sprintf("%x", sha256.Sum256([]byte(fmt.Sprintf("%s:%d", prevBlockHash, block.BlockNumber))))
 	}
-	close(blockWriter) 
+	close(blockWriter)
 
-	for
-	{
-	var blockNumber int
-	fmt.Print("Enter the block number: ")
-	_, err = fmt.Scanln(&blockNumber)
-	if err != nil {
-		log.Fatal("Error reading block number:", err)
-	}
-	fetchedBlock, err := fetchBlockDetailsByNumber(blockNumber)
-	if err != nil {
-		fmt.Println("Error fetching block details:", err)
-		return
-	}
-	fmt.Println("Fetched Block Details:")
-	fmt.Println("Block Number:", fetchedBlock.BlockNumber)
-	fmt.Println("Prev Block Hash:", fetchedBlock.PrevBlockHash)
-	fmt.Println("Block Status:", fetchedBlock.Status)
-	fmt.Println("Processing Time:", fetchedBlock.ProcessingTime)
-	fmt.Printf("Transactions:\n")
-	for txnID, txn := range fetchedBlock.Txns {
-		fmt.Printf("  TxnID: %s\n", txnID)
-		fmt.Printf("  IsValid: %t\n", txn.IsValid)
-		fmt.Printf("  Data:\n")
-		for key, entry := range txn.Data {
-			fmt.Printf("    Key: %s\n", key)
-			fmt.Printf("    Value: %v\n", entry)
+	for {
+		var blockNumber int
+		fmt.Print("Enter the block number: ")
+		_, err = fmt.Scanln(&blockNumber)
+		if err != nil {
+			log.Fatal("Error reading block number:", err)
 		}
-	}
-	var input string
+		fetchedBlock, err := fetchBlockDetailsByNumber(blockNumber)
+		if err != nil {
+			fmt.Println("Error fetching block details:", err)
+			return
+		}
+		fmt.Println("Fetched Block Details:")
+		fmt.Println("Block Number:", fetchedBlock.BlockNumber)
+		fmt.Println("Prev Block Hash:", fetchedBlock.PrevBlockHash)
+		fmt.Println("Block Status:", fetchedBlock.Status)
+		fmt.Println("Processing Time:", fetchedBlock.ProcessingTime)
+		fmt.Printf("Transactions:\n")
+		for txnID, txn := range fetchedBlock.Txns {
+			fmt.Printf("  TxnID: %s\n", txnID)
+			fmt.Printf("  IsValid: %t\n", txn.IsValid)
+			fmt.Printf("  Data:\n")
+			for key, entry := range txn.Data {
+				fmt.Printf("    Key: %s\n", key)
+				fmt.Printf("    Value: %v\n", entry)
+			}
+		}
+		var input string
 		fmt.Print("Do you want to fetch another block? (yes/no): ")
 		_, err = fmt.Scanln(&input)
 		if err != nil {
@@ -330,9 +322,21 @@ func main() {
 				fmt.Println("    Hash:", entry.Hash)
 			}
 		}
-}
+	}
 
 	endTime := time.Now()
 	elapsedTime := endTime.Sub(startTime)
 	fmt.Println("\nTotal Execution Time:", elapsedTime)
+}
+func getEnvInt(key string, defaultValue int) int {
+	valueStr := os.Getenv(key)
+	if valueStr == "" {
+		return defaultValue
+	}
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		fmt.Printf("Invalid value for environment variable %s, using default value\n", key)
+		return defaultValue
+	}
+	return value
 }
